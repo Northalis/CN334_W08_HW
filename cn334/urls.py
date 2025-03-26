4641c5b893de39ef27fefa8e66260440e10c13d2
Northalis <peatwtp@gmail.com>
2025-03-26 13:57:41 +0700

    Add ecom & template for data

--- cn334/urls.py
+++ cn334/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from ecommerce import views as ecommerce_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("ecommerce/", ecommerce_views.ecommerce_index),
+    path("ecommerce/item/<item_id>", ecommerce_views.ecommerce_index),
 ]
